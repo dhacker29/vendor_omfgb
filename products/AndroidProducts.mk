f6170e2df1b75276dc9ca4ffa22dc8ed772e0605
dhacker29 <davidhackerdvm@gmail.com>
2011-08-31 02:48:16 -0500

    Edited products/AndroidProducts.mk via GitHub

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -8,7 +8,7 @@ else ifeq ($(TARGET_PRODUCT),OMFGB_supersonic)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/omfgb_supersonic.mk
 else ifeq ($(TARGET_PRODUCT),OMFGB_inc)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/omfgb_inc.mk
-else ifeq ($(TARGET_PRODUCT),SHIFTAO5P)
+else ifeq ($(TARGET_PRODUCT),OMFGB_mecha)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/omfgb_mecha.mk
 else ifeq ($(TARGET_PRODUCT),OMFGB_vivow)
   PRODUCT_MAKEFILES := $(LOCAL_DIR)/omfgb_vivow.mk
